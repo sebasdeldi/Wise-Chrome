153a773d4614515cd4ba2cd371458cb648284711
Sebastián Delgao <sebasdeldi123@gmail.com>
2016-12-10 14:32:23 -0500

    icons changed

--- newtab.js
+++ newtab.js
@@ -6,7 +6,6 @@ String.prototype.replaceAt=function(index, character) {
     return this.substr(0, index) + character + this.substr(index+character.length);
 }
 
-
 //Todo List Script
 function get_todos() {
     var todos = new Array;
